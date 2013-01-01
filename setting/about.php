7d16ef2865ebf7282713dc4c556f9d89b6b806f2
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-01 16:08:27 +0000

    commit new files.
    git-svn-id: https://plugins.svn.wordpress.org/ir-sms/trunk@646687 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ setting/about.php
@@ -0,0 +1,8 @@
+<div class="wrap">
+		<h2><?php _e('About Persian SMS Plugin', 'wp-sms'); ?></h2>
+		<center><img src="<?php bloginfo('url'); ?>/wp-content/plugins/ir-sms/images/logo.png"/></center>
+		<p><?php echo sprintf(__('Version plugin: %s', 'wp-sms'), WP_SMS_VERSION); ?></p>
+		
+		<p><?php echo sprintf(__('HTML ABOUT PLUGIN', 'wp-sms'), 'http://www.webstudio.ir'); ?></p>
+		<p style="font-size:10px;"><?php echo sprintf(__('Programmers and developers: %s.', 'wp-sms'), '<code><a href="http://iran98.org">M Soufi</a></code>'); ?></p>
+	</div>
